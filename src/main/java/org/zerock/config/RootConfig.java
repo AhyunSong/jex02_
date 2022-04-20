e957df641cbf68530a21012650b21d2ca6bae20d
songahyun <1thddkgus96@gmail.com>
2022-04-20 12:07:01 +0900

    jex02 신규 생성

---
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,41 @@
+package org.zerock.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
+public class RootConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        //hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+        //hikariConfig.setJdbcUrl("jdbc:oracle:thin:@183.98.24.70:51522:orcl11");
+        hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+        hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@183.98.24.70:51522:orcl11");
+
+        hikariConfig.setUsername("book_ex");
+        hikariConfig.setPassword("book_ex");
+
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+        return dataSource;
+    }
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource());
+        return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
+}
+
